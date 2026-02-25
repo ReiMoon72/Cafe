9a8c92694175c21d297f2e1f10fc08ad080e0b47
ReiMoon72 <rhelpalma6@gmail.com>
2026-02-25 19:23:34 +0800

    About US section Complete

--- src/views/About.vue
+++ src/views/About.vue
@@ -56,8 +56,10 @@ export default {
     <h1><b>Our Mission & Vision</b></h1>
   </div>
 
-  <div>
-    <div class="bg-cyan-800 p-2 w-75">
+  <div
+    class="flex justify-evenly items-center p-5 text-center flex-col gap-3 text-white md:flex-row"
+  >
+    <div class="bg-cyan-800 p-2 w-75 rounded-md">
       <h2 class="text-3xl"><b>Mission</b></h2>
       <p>
         To serve high-quality coffee and freshly baked goods while creating a
@@ -65,12 +67,39 @@ export default {
         welcoming space for community and connection. <br />
       </p>
     </div>
-    <div class="bg-cyan-800 p-2 w-75">
+    <div class="bg-cyan-800 p-2 w-75 h-35 rounded-md">
       <h2 class="text-3xl"><b>Vision</b></h2>
       <p>
-        To become the go-to neighborhood cafe known for comfort, creativity, and <br>
+        To become the go-to neighborhood cafe known for comfort, creativity, and
+        <br />
         unforgettable flavors.
       </p>
+    </div>
+  </div>
+
+  <!--Why you should choose us-->
+
+  <div class="p-10 text-3xl flex justify-center items-center">
+    <h1><b>Why you should choose us?</b></h1>
+  </div>
+
+  <!--The wraps-->
+
+  <div
+    class="flex justify-evenly items-center p-3 flex-col gap-3 text-center md:flex-row"
+  >
+    <!--kape-->
+    <div class="bg-stone-400 w-70 p-3 rounded-md">
+      <h1><b>Local Producers</b></h1>
+      <p>Localy source from our local Farmers</p>
+    </div>
+    <div class="bg-stone-400 w-70 p-3 rounded-md">
+      <h1><b>Fresh Bake Bread Daily</b></h1>
+      <p>Delicious treats and bake in house every morning</p>
+    </div>
+    <div class="bg-stone-400 w-70 p-6 rounded-md">
+      <h1><b>Cosy & Free internet</b></h1>
+      <p>Comfort space with free WIFI.</p>
     </div>
   </div>
 </template>
